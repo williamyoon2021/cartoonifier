0dae77e32a9ec01f808b608e0c2843e45f14f97d
William Yoon <89663919+wty5@users.noreply.github.com>
2022-02-12 02:29:07 -0500

    finished

--- cartoonifier-python-project.py
+++ cartoonifier-python-project.py
@@ -18,10 +18,11 @@ top.configure(background='white')
 label=Label(top,background='#CDCDCD', font=('calibri',20,'bold'))
 
 def upload():
-    ImagePath=easygui.fileopenbox() #method in easyGUI that returns path of chosen file as string
+    ImagePath=easygui.fileopenbox() 
     cartoonify(ImagePath)
+    #method in easyGUI that returns path of chosen file as string
 
-def cartoofify(ImagePath):
+def cartoonify(ImagePath):
     #read image
     originalmage = cv2.imread(ImagePath) #store images in form of numbers
     originalmage = cv2.cvtColor(originalmage,cv2.COLOR_BGR2RGB)
@@ -45,9 +46,7 @@ def cartoofify(ImagePath):
     #plt.imshow(ReSized3, cmap='gray')
 
     #retreiving edges for cartoon effect using thresholding technique
-    getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255,
-        cv2.ADAPTIVE_THRESH_MEAN_C,
-        cv2.THRESH_BINARY,9,9)
+    getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,9,9)
 
     ReSized4 = cv2.resize(getEdge, (960,540))
     #plt.imshow(ReSized4, cmap='gray')
@@ -58,17 +57,18 @@ def cartoofify(ImagePath):
     #plt.imshow(ReSized5, cmap='gray)
 
     #masking edged image with "BEAUTIFY" image
-    cartoonImage = cv.bitwise_and(colorImage, colorImage, mask=getEdge)
+    cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge)
     ReSized6 = cv2.resize(cartoonImage, (960, 540))
     #plt.imshow(ReSized6, cmap='gray')
 
     #Plotting whole transition
     images=[ReSized1,ReSized2,ReSized3,ReSized4,ReSized5,ReSized6]
+
     fig, axes = plt.subplots(3,2, figsize=(8,8), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.1,wspace=0.1))
     for i, ax in enumerate(axes.flat):
         ax.imshow(images[i], cmap='gray')
 
-    save1=Button(top,text="Save cartoon image", command=lambda:save(ImagePath,ReSized6),padx=30,pady=5)
+    save1=Button(top,text="Save cartoon image", command=lambda:save(ReSized6, ImagePath),padx=30,pady=5)
     save1.configure(background='#364156',foreground='white',font=('calibri',10,'bold'))
     save1.pack(size=TOP,pady=50)
     
@@ -82,11 +82,11 @@ def save(ReSized6, ImagePath):
     extension = os.path.splitext(ImagePath)[1]
     path = os.path.join(path1, newName+extension)
     cv2.imwrite(path, cv2.cvtColor(ReSized6, cv2.COLOR_RGB2BGR))
-    I = "Image saved by name " + newName + " at " + path
+    I= "Image saved by name " + newName + " at " + path
     tk.messagebox.showinfo(title=None, message=I)
 
 upload=Button(top,text="Cartoonify Image",command=upload,padx=10,pady=5)
-upload.configure(background='#354156',foreground='white', font=('calibri', 10,'bold'))
-upload.pack(size=TOP,pady=50)
+upload.configure(background='#364156',foreground='white', font=('calibri', 10,'bold'))
+upload.pack(side=TOP,pady=50)
 
 top.mainloop()
